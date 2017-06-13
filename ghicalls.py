447c8414e545b6bd993a54fdd8822c498fbd9f42
Ryan Hefner <rhefner1@gmail.com>
2017-06-13 11:18:33 -0400

    Make transition to Python 2.7

--- ghicalls.py
+++ ghicalls.py
@@ -1,81 +1,35 @@
-import os
+import textwrap
 import wsgiref.handlers
-from xml.dom import minidom
 
-from google.appengine.ext.webapp import template
 from google.appengine.ext import webapp
-from google.appengine.api import urlfetch
 
-BASE_URL = "https://ghicalls.appspot.com/"
-
-class ForwardCall(webapp.RequestHandler):
-    """
-    Accepts input digits from the caller and forwards the call
-    """
-    def get(self):
-        self.post()
-    
-    def _error(self, msg, redirecturl=None):
-        templatevalues = {
-            'msg': msg,
-            'redirecturl': redirecturl
-        }
-        xml_response(self, 'error.xml', templatevalues)
-
-    def post(self):
-        forward = self.request.get('Digits')
-        if forward:
-            try:
-                forward = int(forward)
-
-                if forward == 1:
-                    number = "919-438-2444"
-                elif forward == 2:
-                    number = "919-438-2444"
-                else:
-                    raise
-            
-                xml_response(self, 'forward.xml', {"number" : number})
-
-            except:
-                self._error("Sorry, I didn't recognize the number you entered.", BASE_URL)
-
-        else:
-            self._error("Sorry, I didn't recognize the number you entered.", BASE_URL)
-
-
-# @start snippet
-def xml_response(handler, page, templatevalues=None):
-    """
-    Renders an XML response using a provided template page and values
-    """
-    path = os.path.join(os.path.dirname(__file__), page)
-    handler.response.headers["Content-Type"] = "text/xml"
-    handler.response.out.write(template.render(path, templatevalues))
 
 class GatherPage(webapp.RequestHandler):
     """
     Initial user greeting.
     """
+
     def get(self):
         self.post()
-    
+
     def post(self):
-        templatevalues = {
-            'postprefix': BASE_URL,
-        }
-        # xml_response(self, 'gather.xml', templatevalues)
-        xml_response(self, 'directoffice.xml', templatevalues)
-# @end snippet
+        self.response.headers["Content-Type"] = "text/xml"
+        self.response.out.write(textwrap.dedent('''
+        <?xml version="1.0" encoding="UTF-8"?>
+        <Response>
+            <Play>greeting.wav</Play>
+            <Dial timeout="500">919-438-2444</Dial>
+        </Response>
+        '''.strip('\n')))
+
 
 def main():
-	# @start snippet
-    application = webapp.WSGIApplication([ \
-        ('/', GatherPage),
-        ('/forward', ForwardCall)],
+    application = webapp.WSGIApplication([
+        ('/', GatherPage)
+    ],
         debug=True)
-    # @end snippet
     wsgiref.handlers.CGIHandler().run(application)
+
 
 if __name__ == "__main__":
     main()
